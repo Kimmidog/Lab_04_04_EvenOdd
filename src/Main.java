74b9169e98d23f4877712aaa988df4caad0647e1
Kimmidog <Kimmidog4@gmail.com>
2024-09-24 20:15:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        int numToExamine = 2; // program to determine that since 2 mod 2 is 0 it is even
+        // modulo = %
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is even");
+        }
+        else {
+            System.out.println(numToExamine + " is odd");
+        }
+// i think this is right
+    }
+}
